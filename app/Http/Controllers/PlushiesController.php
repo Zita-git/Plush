06bcca0ca8c027d9a3bd252d326843efdfb71d37
Zita <ambrus.zita99@gmail.com>
2022-03-22 14:36:37 +0100

    faker()

---
+++ app/Http/Controllers/PlushiesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plushies;
+use Illuminate\Http\Request;
+
+class PlushiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Plushies  $plushies
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Plushies $plushies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Plushies  $plushies
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Plushies $plushies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Plushies  $plushies
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Plushies $plushies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Plushies  $plushies
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Plushies $plushies)
+    {
+        //
+    }
+}
